edf1266f44944ed535465d1fc98fce9c46844b0d
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-25 12:55:24 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const RichEmbed = new Discord.RichEmbed();
+var fs = require('fs');
+
+// -->COMMANDS<--
+
+bot.on('guildMemberAdd',member =>{
+    member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/b7K4f3e');
+
+    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+
+});
+
+
+bot.on('guildMemberRemove', member=>{
+     member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/b7K4f3e');
+
+    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+  
+});
+
+
+
+bot.on('ready',()=> {
+console.log('Recep Fides');
+
+});
+
+bot.login(BOT_TOKEN);

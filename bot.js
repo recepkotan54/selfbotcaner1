480cb97b1fb6fb6159580540bb8c928e26dfd17e
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-27 18:30:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/UQdBD4N');
+    member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/b7K4f3e');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
@@ -14,7 +14,7 @@ bot.on('guildMemberAdd',member =>{
 
 
 bot.on('guildMemberRemove', member=>{
-     member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/UQdBD4N');
+     member.send('**Biz Büüymekte Olan Bir Discord Sunucusuyuz** https://discord.gg/b7K4f3e');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
   
